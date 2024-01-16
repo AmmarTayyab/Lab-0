ffc123d2748838e9f1ae93e5a8685a79cda7ce75
AmmarTayyab <152747192+AmmarTayyab@users.noreply.github.com>
2024-01-15 23:30:31 -0700

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,34 @@
+﻿// January-13-2024 Ammar Tayyab
+using System;
+
+class Test
+{
+        static void Main()
+        {int lowNum;
+                do
+        { 
+          Console.WriteLine("Enter the lower of two numbers: ");
+          lowNum = Convert.ToInt32(Console.ReadLine());
+        } while (lowNum<0);
+            int highNum;
+        do
+        {Console.WriteLine("Enter the higher of two numbers: ");
+         highNum = Convert.ToInt32(Console.ReadLine());
+        }while (lowNum>=highNum);
+            Console.WriteLine($"You entered the numbers {highNum} and {lowNum}");
+            int numDiff = highNum-lowNum;
+            Console.WriteLine ($"The difference between these numbers is {numDiff}");
+            int[] numberArray = new int[numDiff];
+            for (int i = 1; i < numberArray.Length; i++)
+            {
+            numberArray[i] = lowNum + i;
+            }
+        using (StreamWriter writer = new StreamWriter("numbers.txt"))
+        {
+                foreach (int num in numberArray)
+            {
+                writer.WriteLine(num);
+            }
+        }
+        }
+}
